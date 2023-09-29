f1c53cec6fc1b9d3216443620952c3edd646f546
ngc-thanh <thban2803@gmail.com>
2023-09-30 08:16:50 +0900

    implement facebook login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import React, { useState, useEffect } from "react";
+import FacebookLogin from "react-facebook-login";
+
 import "./App.css";
 
+const FACEBOOK_ID = process.env.REACT_APP_FACEBOOK_ID;
+
 function App() {
+  const [fbLogin, setFBLogin] = useState(false);
+  const [fbData, setDataFB] = useState({});
+
+  const responseFacebook = (response) => {
+    setDataFB(response);
+    if (response.accessToken) {
+      setFBLogin(true);
+    } else {
+      setFBLogin(false);
+    }
+  };
+
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {!fbLogin && (
+        <FacebookLogin
+          appId={FACEBOOK_ID}
+          autoLoad={true}
+          fields="name,email,picture"
+          scope="public_profile,user_friends"
+          callback={responseFacebook}
+          icon="fa-facebook"
+          className="my-2"
+        />
+      )}
+
+      {fbLogin && (
+        <div className="my-2">
+          <h1 className="text-3xl font-semibold text-center mt-2">
+            Hello, {fbData.name}
+          </h1>
+        </div>
+      )}
     </div>
   );
 }
