1be01c4c5b93750cde8da7c5e189c1b08ee4cacf
ngc-thanh <thban2803@gmail.com>
2023-09-30 11:08:03 +0900

    edit text in button

--- src/components/Event.js
+++ src/components/Event.js
@@ -84,7 +84,7 @@ const Event = ({
             className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
             onClick={handleCancelClick}
           >
-            HUỶ
+            KHÔNG THAM GIA
           </button>
         ) : (
           <button
