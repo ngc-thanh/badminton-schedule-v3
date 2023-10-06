9ed45132ba699fd4969fc1dee815f2c1790f6e54
n-thanh <thban2803@gmail.com>
2023-10-06 17:08:01 +0900

    add EditEvent component

---
+++ src/components/EditEvent.js
@@ -0,0 +1,85 @@
+import EditEventModal from "./EditEventModal";
+import { useState, useEffect } from "react";
+import {
+  doc,
+  updateDoc,
+  Timestamp,
+} from "firebase/firestore";
+import { db } from "../firebase";
+
+function EditEvent({ onClose, open, event }) {
+  const [amount, setAmount] = useState(event.data.amount);
+  const [participant, setParticipant] = useState(event.data.participant);
+
+  /* function to add new task to firestore */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const eventDocRef = doc(db, "events", event.id);
+    try {
+      await updateDoc(eventDocRef, {
+        amount: amount,
+        participant: participant,
+        updated: Timestamp.now(),
+      });
+
+      onClose();
+    } catch (err) {
+      alert("Edit event failed: " + err);
+    }
+  };
+
+  return (
+    <EditEventModal modalLabel="SỬA SÂN" onClose={onClose} open={open}>
+      <form onSubmit={handleSubmit}>
+        <div className="grid grid-cols-2 gap-4 mb-2">
+          <div className="mb-1">
+            <label
+              htmlFor="amount"
+              className="block text-gray-700 font-semibold text-left mb-1"
+            >
+              Số sân
+            </label>
+            <input
+              id="amount"
+              required
+              type="number"
+              onChange={(e) => {
+                setAmount(e.target.value);
+              }}
+              placeholder="3"
+              value={amount}
+              rows="4"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:outline-none h-12"
+            ></input>
+          </div>
+          <div className="mb-1">
+            <label
+              htmlFor="participant"
+              className="block text-gray-700 font-semibold text-left mb-1"
+            >
+              Giới hạn số người
+            </label>
+            <input
+              id="participant"
+              required
+              type="number"
+              onChange={(e) => setParticipant(e.target.value)}
+              placeholder="16"
+              value={participant}
+              rows="4"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:outline-none h-12"
+            ></input>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md font-semibold transition duration-300 ease-in-out"
+        >
+          SỬA
+        </button>
+      </form>
+    </EditEventModal>
+  );
+}
+
+export default EditEvent;
---
+++ src/components/EditEventModal.js
@@ -0,0 +1,34 @@
+function EditEventModal({ open, modalLabel, children, custom_modal, onClose }) {
+    const handleClose = (e) => {
+      if (e.target.className === "modalContainer") {
+        onClose();
+      }
+      return null;
+    };
+  
+    if (open) {
+      return (
+        <div
+          onClick={handleClose}
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+        >
+          <div className="bg-white p-4 rounded-lg shadow-md overflow-y-auto w-11/12 md:w-2/4 h-3/4 md:h-max">
+            <div className="flex justify-between items-center mb-2">
+              <h2 className="text-xl font-semibold">{modalLabel}</h2>
+              <span
+                onClick={onClose}
+                className="cursor-pointer text-gray-600 hover:text-gray-800 text-lg"
+              >
+                x
+              </span>
+            </div>
+            {children}
+          </div>
+        </div>
+      );
+    }
+    return null;
+  }
+  
+  export default EditEventModal;
+  
